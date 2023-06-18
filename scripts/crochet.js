2f7cc06607e97874ebb05d62823da9f7ef77daab
codelazarus14 <codel1763@gmail.com>
2023-06-18 11:41:37 -0500

    added add/delete materials buttons

--- scripts/crochet.js
+++ scripts/crochet.js
@@ -6,24 +6,94 @@ class Pattern {
   }
 }
 
-const patterns = [new Pattern('US Crochet')];
-const patternSelectElement = document.querySelector('.js-pattern-types');
+class CrochetPattern extends Pattern {
+  constructor() {
+    super('US Crochet');
+    this.hookSizes = ['US L'];
+    this.hooks = [];
+    this.yarns = [];
+  }
+}
 
+const patterns = [new CrochetPattern()];
+let selectedPattern = patterns[0];
+
+const patternSelectElement = document.querySelector('.js-pattern-types');
+const hookInputElement = document.querySelector('.js-hook-types');
+const hookConfirmInputElement = document.querySelector('.js-hook-confirm');
+const hookListElement = document.querySelector('.js-hook-list');
+const yarnNameInputElement = document.querySelector('.js-yarn-name');
+const yarnAmtInputElement = document.querySelector('.js-yarn-amt');
+const yarnConfirmInputElement = document.querySelector('.js-yarn-confirm');
+const yarnListElement = document.querySelector('.js-yarn-list');
+
+// TODO - replace with better events
 patternSelectElement.addEventListener('click', () => {
   document.querySelector('.js-pattern-body').classList.remove('is-hidden');
+  selectedPattern = patterns[patternSelectElement.selectedIndex];
 });
-renderProjectOptions();
+hookConfirmInputElement.addEventListener('click', () => {
+  const idx = hookInputElement.selectedIndex;
+  const hook = hookInputElement.options[idx].text;
+  selectedPattern.hooks.push(hook);
+  renderHooks();
+});
+yarnConfirmInputElement.addEventListener('click', () => {
+  const yarnName = yarnNameInputElement.value;
+  const yarnAmt = Number(yarnAmtInputElement.value);
+  selectedPattern.yarns.push([yarnName, yarnAmt]);
+  renderYarn();
+});
+const renderPattern = () => renderElement(patternSelectElement, patterns, generatePatternHTML);
+const renderHooks = () => renderElement(hookListElement, selectedPattern.hooks, generateHookHTML);
+const renderYarn = () => renderElement(yarnListElement, selectedPattern.yarns, generateYarnHTML);
 
-function renderProjectOptions() {
-  let projectOptionsHTML = '';
+renderPattern();
+renderHooks();
+renderYarn();
 
-  patterns.forEach(pattern => {
-    const patternType = pattern.type;
-    const html = `<option value="
-        ${patternType.toLowerCase()}
-      ">${patternType}</option>`;
-    projectOptionsHTML += html;
+
+
+function renderElement(element, elementData, htmlGenerator) {
+  let html = '';
+
+  elementData.forEach((item, index) => {
+    html += htmlGenerator(item, index);
   });
 
-  patternSelectElement.innerHTML = projectOptionsHTML;
+  element.innerHTML = html;
+
+  document.querySelectorAll('.js-delete-hook-button')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        selectedPattern.hooks.splice(index, 1);
+        renderHooks();
+      });
+    });
+  
+    document.querySelectorAll('.js-delete-yarn-button')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        selectedPattern.yarns.splice(index, 1);
+        renderYarn();
+      });
+    });
+}
+
+function generatePatternHTML(pattern, index) {
+  const patternType = pattern.type;
+  return `<option value="${patternType.toLowerCase()}">
+  ${patternType}</option>`;
+}
+
+function generateHookHTML(hookSize, index) {
+  return `<p>${index + 1}.</p>
+  <p>${hookSize}</p>
+  <button class="js-delete-hook-button">-</button>`;
+}
+
+function generateYarnHTML(yarn, index) {
+  return `<p>${yarn[0]}</p>
+  <input class="yarn-amt" type="number" value="${yarn[1]}"><p>skeins</p>
+  <button class="js-delete-yarn-button">-</button>`;
 }
